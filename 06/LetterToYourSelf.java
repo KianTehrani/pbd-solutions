854db4cd71fc1f094464128f4675b859e35a6614
KianTehrani <36419464+KianTehrani@users.noreply.github.com>
2018-02-16 14:31:15 -0500

    Add files via upload

---
+++ 06/LetterToYourSelf.java
@@ -0,0 +1,14 @@
+public class LetterToYourSelf {
+	public static void main(String[] args) {
+	System.out.println("+---------------------------------------------------------+");
+	System.out.println("|                                                    #### |");
+	System.out.println("|                                                    #### |");
+	System.out.println("|                                                    #### |");
+	System.out.println("|                                                         |");
+	System.out.println("|                                                         |");
+	System.out.println("|                              Kian Tehrani               |");
+	System.out.println("|                              7 Money Way                |");
+	System.out.println("|                              Bank, ON 74259             |");
+	System.out.println("|                                                         |");
+	System.out.println("+---------------------------------------------------------+"); }
+}
---
+++ 08/NumbersAndMath.java
@@ -0,0 +1,25 @@
+public class NumbersAndMath {
+	public static void main(String[] args) {
+	System.out.println("Now I will count my money.");
+	System.println("$20" + (25 + 30 - 5) );
+	System.println("$100" + (50 - 10 + 40 * 5 % 7) );
+
+	System.out.println("Now I'll count my cheques.");
+	System.out.println( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 );
+
+	System.out.println( "Is it true that 3 + 2 < 5 - 7?" );
+
+	System.out.println( 3 + 2 < 5 - 7 );
+
+	System.out.println( "What is 3 + 2? " + ( 3 + 2 ) );
+	System.out.println( "What is 5 - 7? " + ( 5 - 7 ) );
+
+	System.out.println( "Oh, that's why it's false." );
+
+	System.out.println( "How about some more." );
+
+	System.out.println( "Is it greater? " + ( 5 > -2 ) );
+	System.out.println( "Is it greater or equal? " + ( 5 >= -2 ) );
+	System.out.println( "Is it less or equal? " + ( 5 <= -2 ) );
+	}
+}
