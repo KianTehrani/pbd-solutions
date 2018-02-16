d75532504c4aff99c73c389039f4eff93dde29fd
KianTehrani <36419464+KianTehrani@users.noreply.github.com>
2018-02-16 14:32:10 -0500

    Add files via upload

---
+++ 04/GoodFirstProg.java
@@ -0,0 +1,12 @@
+public class GoodFirstProg {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello Again.");
+		System.out.println("I like tyoing this.");
+		System.out.println("This is Fun!");
+		System.out.println("Yay! Printing.");
+		System.out.println("I'd much rather you 'not'.");
+		System.out.println("I \"said\" do not touch this."); 
+	}
+}
+
